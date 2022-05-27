267a12da4ff2033a6a93de133e886eca00ba941b
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-05-27 07:45:02 +0300

    The Number of Tickets is now inside the venue.tsx

--- app/javascript/components/venue.tsx
+++ app/javascript/components/venue.tsx
@@ -6,7 +6,13 @@ interface VenueProps {
   seatsPerRow: number
 }
 
+interface VenueInitialState {
+  numberOfTickets: number
+}
+
 const Venue = (props: VenueProps): React.ReactElement => {
+  const [state, setState] = React.useState(Venue.initialState)
+
   const rows = Array.from(Array(props.rows).keys()).map(
     (row: number): React.ReactElement => {
       return (
@@ -19,7 +25,38 @@ const Venue = (props: VenueProps): React.ReactElement => {
     }
   )
 
-  return <table className="table" cellPadding={20}>{rows}</table>
+  const options = Array.from(Array(props.seatsPerRow).keys()).map(
+    (optionValue: number): React.ReactElement => (
+      <option key={`number-of-tickets-${optionValue}`} value={optionValue + 1} selected={state.numberOfTickets == optionValue + 1}>
+        {optionValue + 1}
+      </option>
+    )
+  )
+
+  const numberOfTicketsChanged = (inputElement) => {
+    setState({
+      ...state,
+      numberOfTickets: parseInt(inputElement.target.value),
+    })
+  }
+
+  return (
+    <>
+      <div>
+        <span className="header">How many tickets would you like?</span>
+        <span className="select">
+          <select onChange={numberOfTicketsChanged}>{options}</select>
+        </span>
+      </div>
+      <table className="table" cellPadding={20}>
+        {rows}
+      </table>
+    </>
+  )
+}
+
+Venue.initialState = {
+  numberOfTickets: 2,
 }
 
 export default Venue
