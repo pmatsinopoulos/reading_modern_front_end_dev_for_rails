b78c59842f0b5f215f720ba6f42ac7ddc083e3ef
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-05-31 08:04:54 +0300

    I can now click outside of the search results and the modal with results closes

--- app/javascript/controllers/search_controller.ts
+++ app/javascript/controllers/search_controller.ts
@@ -2,11 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 import "form-request-submit-polyfill"
 
 export default class SearchController extends Controller {
-  static targets = ["form", "input"]
+  static targets = ["form", "input", "results"]
   formTarget: HTMLFormElement
   inputTarget: HTMLInputElement
+  resultsTarget: HTMLElement
 
   submit(): void {
     this.formTarget.requestSubmit()
+  }
+
+  resetOnOutsideClick(event: Event): void {
+    if (!this.element.contains(event.target as HTMLElement)) {
+      this.reset()
+    }
+  }
+
+  private
+
+  reset(): void {
+    this.resultsTarget.classList.add("hidden")
+    this.resultsTarget.innerText = ""
+    this.inputTarget.value = ""
   }
 }
